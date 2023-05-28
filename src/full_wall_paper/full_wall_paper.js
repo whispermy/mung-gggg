7b98ebc520f45a16e03503c384272671677ebbbd
whispermy <whispermy@github.com>
2023-05-29 03:21:26 +0900

    [수정] 이미지 전체화면 표시

--- src/full_wall_paper/full_wall_paper.js
+++ src/full_wall_paper/full_wall_paper.js
@@ -1,13 +1,29 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import { PropagateLoader } from "react-spinners";
-import ProgressiveImg from "./ProgressiveImg";
+import { styled } from "styled-components";
 
 export default function Loading02() {
   const imgsrc = useLocation();
   const url = imgsrc.state.data;
+  const Container = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-image: url(${url}/full.jpg);
+  background-size: cover;
+  background-repeat: no-repeat;
+  `;
+
   return (
     <div>
+      <Container>
+        {/* <ProgressiveImg
+          src={url}
+        /> */}
+      </Container>
       <Link to="/">
         <h3>Please wating...</h3>
         <PropagateLoader color="#36d7b7"
@@ -16,15 +32,6 @@ export default function Loading02() {
         speedMultiplier={0.8}
         />
       </Link>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      
-      <ProgressiveImg
-        src={url}
-      />
     </div>
   );
 };
