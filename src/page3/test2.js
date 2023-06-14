f64399f776513554d58cff9f2202cf8e961906c8
minjunss <jomj2214@gmail.com>
2023-06-14 20:27:27 +0900

    background는 일단 url로 대체

--- src/page3/test2.js
+++ src/page3/test2.js
@@ -1,12 +1,12 @@
-import { gsap } from 'gsap';
-import imagesLoaded from 'imagesloaded';
-import React, { useEffect } from 'react';
+import { gsap } from "gsap";
+import imagesLoaded from "imagesloaded";
+import React, { useEffect } from "react";
 
 const CardComponent = () => {
-    useEffect(() => {
+  useEffect(() => {
     const buttons = {
-        prev: document.querySelector(".btn--left"),
-        next: document.querySelector(".btn--right"),
+      prev: document.querySelector(".btn--left"),
+      next: document.querySelector(".btn--right"),
     };
 
     const cardsContainerEl = document.querySelector(".cards__wrapper");
@@ -18,166 +18,177 @@ const CardComponent = () => {
     buttons.prev.addEventListener("click", () => swapCards("left"));
 
     function updateCard(e) {
-        // ... (updateCard 함수 내용을 그대로 유지)
-        const card = e.currentTarget;
-        const box = card.getBoundingClientRect();
-        const centerPosition = {
-            x: box.left + box.width / 2,
-            y: box.top + box.height / 2,
-        };
-        let angle = Math.atan2(e.pageX - centerPosition.x, 0) * (35 / Math.PI);
-        gsap.set(card, {
-            "--current-card-rotation-offset": `${angle}deg`,
-        });
-        const currentInfoEl = cardInfosContainerEl.querySelector(".current--info");
-        gsap.set(currentInfoEl, {
-            rotateY: `${angle}deg`,
-        });
+      // ... (updateCard 함수 내용을 그대로 유지)
+      const card = e.currentTarget;
+      const box = card.getBoundingClientRect();
+      const centerPosition = {
+        x: box.left + box.width / 2,
+        y: box.top + box.height / 2,
+      };
+      let angle = Math.atan2(e.pageX - centerPosition.x, 0) * (35 / Math.PI);
+      gsap.set(card, {
+        "--current-card-rotation-offset": `${angle}deg`,
+      });
+      const currentInfoEl =
+        cardInfosContainerEl.querySelector(".current--info");
+      gsap.set(currentInfoEl, {
+        rotateY: `${angle}deg`,
+      });
     }
 
     function swapCards(direction) {
-        // ... (swapCards 함수 내용을 그대로 유지)
-        const currentCardEl = cardsContainerEl.querySelector(".current--card");
-        const previousCardEl = cardsContainerEl.querySelector(".previous--card");
-        const nextCardEl = cardsContainerEl.querySelector(".next--card");
+      // ... (swapCards 함수 내용을 그대로 유지)
+      const currentCardEl = cardsContainerEl.querySelector(".current--card");
+      const previousCardEl = cardsContainerEl.querySelector(".previous--card");
+      const nextCardEl = cardsContainerEl.querySelector(".next--card");
 
-        const currentBgImageEl = appBgContainerEl.querySelector(".current--image");
-        const previousBgImageEl = appBgContainerEl.querySelector(".previous--image");
-        const nextBgImageEl = appBgContainerEl.querySelector(".next--image");
+      const currentBgImageEl =
+        appBgContainerEl.querySelector(".current--image");
+      const previousBgImageEl =
+        appBgContainerEl.querySelector(".previous--image");
+      const nextBgImageEl = appBgContainerEl.querySelector(".next--image");
 
-        changeInfo(direction);
-        swapCardsClass();
+      changeInfo(direction);
+      swapCardsClass();
 
-        removeCardEvents(currentCardEl);
+      removeCardEvents(currentCardEl);
 
-        function swapCardsClass() {
-            currentCardEl.classList.remove("current--card");
-            previousCardEl.classList.remove("previous--card");
-            nextCardEl.classList.remove("next--card");
+      function swapCardsClass() {
+        currentCardEl.classList.remove("current--card");
+        previousCardEl.classList.remove("previous--card");
+        nextCardEl.classList.remove("next--card");
 
-            currentBgImageEl.classList.remove("current--image");
-            previousBgImageEl.classList.remove("previous--image");
-            nextBgImageEl.classList.remove("next--image");
+        currentBgImageEl.classList.remove("current--image");
+        previousBgImageEl.classList.remove("previous--image");
+        nextBgImageEl.classList.remove("next--image");
 
-            currentCardEl.style.zIndex = "50";
-            currentBgImageEl.style.zIndex = "-2";
+        currentCardEl.style.zIndex = "50";
+        currentBgImageEl.style.zIndex = "-2";
 
-            if (direction === "right") {
-                previousCardEl.style.zIndex = "20";
-                nextCardEl.style.zIndex = "30";
+        if (direction === "right") {
+          previousCardEl.style.zIndex = "20";
+          nextCardEl.style.zIndex = "30";
 
-                nextBgImageEl.style.zIndex = "-1";
+          nextBgImageEl.style.zIndex = "-1";
 
-                currentCardEl.classList.add("previous--card");
-                previousCardEl.classList.add("next--card");
-                nextCardEl.classList.add("current--card");
+          currentCardEl.classList.add("previous--card");
+          previousCardEl.classList.add("next--card");
+          nextCardEl.classList.add("current--card");
 
-                currentBgImageEl.classList.add("previous--image");
-                previousBgImageEl.classList.add("next--image");
-                nextBgImageEl.classList.add("current--image");
-            } else if (direction === "left") {
-                previousCardEl.style.zIndex = "30";
-                nextCardEl.style.zIndex = "20";
+          currentBgImageEl.classList.add("previous--image");
+          previousBgImageEl.classList.add("next--image");
+          nextBgImageEl.classList.add("current--image");
+        } else if (direction === "left") {
+          previousCardEl.style.zIndex = "30";
+          nextCardEl.style.zIndex = "20";
 
-                previousBgImageEl.style.zIndex = "-1";
+          previousBgImageEl.style.zIndex = "-1";
 
-                currentCardEl.classList.add("next--card");
-                previousCardEl.classList.add("current--card");
-                nextCardEl.classList.add("previous--card");
+          currentCardEl.classList.add("next--card");
+          previousCardEl.classList.add("current--card");
+          nextCardEl.classList.add("previous--card");
 
-                currentBgImageEl.classList.add("next--image");
-                previousBgImageEl.classList.add("current--image");
-                nextBgImageEl.classList.add("previous--image");
-            }
+          currentBgImageEl.classList.add("next--image");
+          previousBgImageEl.classList.add("current--image");
+          nextBgImageEl.classList.add("previous--image");
         }
+      }
 
-        function changeInfo(direction) {
+      function changeInfo(direction) {
         // ... (changeInfo 함수 내용을 그대로 유지)
-        let currentInfoEl = cardInfosContainerEl.querySelector(".current--info");
-        let previousInfoEl = cardInfosContainerEl.querySelector(".previous--info");
+        let currentInfoEl =
+          cardInfosContainerEl.querySelector(".current--info");
+        let previousInfoEl =
+          cardInfosContainerEl.querySelector(".previous--info");
         let nextInfoEl = cardInfosContainerEl.querySelector(".next--info");
 
-        gsap.timeline()
-            .to([buttons.prev, buttons.next], {
-                duration: 0.2,
-                opacity: 0.5,
-                pointerEvents: "none",
-            })
-            .to(currentInfoEl.querySelectorAll(".text"), {
-                duration: 0.4,
-                stagger: 0.1,
-                translateY: "-120px",
-                opacity: 0,
-            }, "-=" )
-            .call(() => {
-                swapInfosClass(direction);
-            })
-            .call(() => initCardEvents())
-            .fromTo(
-                direction === "right"
-                ? nextInfoEl.querySelectorAll(".text")
-                : previousInfoEl.querySelectorAll(".text"),
-                {
-                    opacity: 0,
-                    translateY: "40px",
-                },
-                {
-                    duration: 0.4,
-                    stagger: 0.1,
-                    translateY: "0px",
-                    opacity: 1,
-                }
-            )
-            .to([buttons.prev, buttons.next], {
-                duration: 0.2,
-                opacity: 1,
-                pointerEvents: "all",
-            });
-
-            function swapInfosClass() {
-                currentInfoEl.classList.remove("current--info");
-                previousInfoEl.classList.remove("previous--info");
-                nextInfoEl.classList.remove("next--info");
-
-                if (direction === "right") {
-                    currentInfoEl.classList.add("previous--info");
-                    nextInfoEl.classList.add("current--info");
-                    previousInfoEl.classList.add("next--info");
-                } else if (direction === "left") {
-                    currentInfoEl.classList.add("next--info");
-                    nextInfoEl.classList.add("previous--info");
-                    previousInfoEl.classList.add("current--info");
-                }
+        gsap
+          .timeline()
+          .to([buttons.prev, buttons.next], {
+            duration: 0.2,
+            opacity: 0.5,
+            pointerEvents: "none",
+          })
+          .to(
+            currentInfoEl.querySelectorAll(".text"),
+            {
+              duration: 0.4,
+              stagger: 0.1,
+              translateY: "-120px",
+              opacity: 0,
+            },
+            "-="
+          )
+          .call(() => {
+            swapInfosClass(direction);
+          })
+          .call(() => initCardEvents())
+          .fromTo(
+            direction === "right"
+              ? nextInfoEl.querySelectorAll(".text")
+              : previousInfoEl.querySelectorAll(".text"),
+            {
+              opacity: 0,
+              translateY: "40px",
+            },
+            {
+              duration: 0.4,
+              stagger: 0.1,
+              translateY: "0px",
+              opacity: 1,
             }
-        }
+          )
+          .to([buttons.prev, buttons.next], {
+            duration: 0.2,
+            opacity: 1,
+            pointerEvents: "all",
+          });
 
-        function resetCardTransforms(e) {
-            // ... (resetCardTransforms 함수 내용을 그대로 유지)
-            const card = e.currentTarget;
-            const currentInfoEl = cardInfosContainerEl.querySelector(".current--info");
-            gsap.set(card, {
-                "--current-card-rotation-offset": 0,
-            });
-            gsap.set(currentInfoEl, {
-                rotateY: 0,
-            });
-        }
+        function swapInfosClass() {
+          currentInfoEl.classList.remove("current--info");
+          previousInfoEl.classList.remove("previous--info");
+          nextInfoEl.classList.remove("next--info");
 
-        function initCardEvents() {
-            // ... (initCardEvents 함수 내용을 그대로 유지)
-            const currentCardEl = cardsContainerEl.querySelector(".current--card");
-            currentCardEl.addEventListener("pointermove", updateCard);
-            currentCardEl.addEventListener("pointerout", (e) => {
-                resetCardTransforms(e);
-            });
+          if (direction === "right") {
+            currentInfoEl.classList.add("previous--info");
+            nextInfoEl.classList.add("current--info");
+            previousInfoEl.classList.add("next--info");
+          } else if (direction === "left") {
+            currentInfoEl.classList.add("next--info");
+            nextInfoEl.classList.add("previous--info");
+            previousInfoEl.classList.add("current--info");
+          }
         }
+      }
 
-        initCardEvents();
+      function resetCardTransforms(e) {
+        // ... (resetCardTransforms 함수 내용을 그대로 유지)
+        const card = e.currentTarget;
+        const currentInfoEl =
+          cardInfosContainerEl.querySelector(".current--info");
+        gsap.set(card, {
+          "--current-card-rotation-offset": 0,
+        });
+        gsap.set(currentInfoEl, {
+          rotateY: 0,
+        });
+      }
+
+      function initCardEvents() {
+        // ... (initCardEvents 함수 내용을 그대로 유지)
+        const currentCardEl = cardsContainerEl.querySelector(".current--card");
+        currentCardEl.addEventListener("pointermove", updateCard);
+        currentCardEl.addEventListener("pointerout", (e) => {
+          resetCardTransforms(e);
+        });
+      }
+
+      initCardEvents();
     }
 
     function removeCardEvents(card) {
       // ... (removeCardEvents 함수 내용을 그대로 유지)
-	    card.removeEventListener("pointermove", updateCard);
+      card.removeEventListener("pointermove", updateCard);
     }
 
     function init() {
@@ -185,76 +196,87 @@ const CardComponent = () => {
 
       // ... (init 함수 내용을 그대로 유지)
       tl.to(cardsContainerEl.children, {
-		delay: 0.15,
-		duration: 0.5,
-		stagger: {
-			ease: "power4.inOut",
-			from: "right",
-			amount: 0.1,
-		},
-		"--card-translateY-offset": "0%",
-	})
-		.to(cardInfosContainerEl.querySelector(".current--info").querySelectorAll(".text"), {
-		delay: 0.5,
-		duration: 0.4,
-		stagger: 0.1,
-		opacity: 1,
-		translateY: 0,
-	})
-		.to(
-		[buttons.prev, buttons.next],
-		{
-			duration: 0.4,
-			opacity: 1,
-			pointerEvents: "all",
-		},
-		"-=0.4"
-	);
+        delay: 0.15,
+        duration: 0.5,
+        stagger: {
+          ease: "power4.inOut",
+          from: "right",
+          amount: 0.1,
+        },
+        "--card-translateY-offset": "0%",
+      })
+        .to(
+          cardInfosContainerEl
+            .querySelector(".current--info")
+            .querySelectorAll(".text"),
+          {
+            delay: 0.5,
+            duration: 0.4,
+            stagger: 0.1,
+            opacity: 1,
+            translateY: 0,
+          }
+        )
+        .to(
+          [buttons.prev, buttons.next],
+          {
+            duration: 0.4,
+            opacity: 1,
+            pointerEvents: "all",
+          },
+          "-=0.4"
+        );
     }
 
     const waitForImages = () => {
       // ... (waitForImages 함수 내용을 그대로 유지)
-        const images = [...document.querySelectorAll("img")];
-        const totalImages = images.length;
-        let loadedImages = 0;
-        const loaderEl = document.querySelector(".loader span");
+      const images = [...document.querySelectorAll("img")];
+      const totalImages = images.length;
+      let loadedImages = 0;
+      const loaderEl = document.querySelector(".loader span");
 
-        gsap.set(cardsContainerEl.children, {
-            "--card-translateY-offset": "100vh",
-        });
-        gsap.set(cardInfosContainerEl.querySelector(".current--info").querySelectorAll(".text"), {
-            translateY: "40px",
-            opacity: 0,
-        });
-        gsap.set([buttons.prev, buttons.next], {
-            pointerEvents: "none",
-            opacity: "0",
-        });
+      gsap.set(cardsContainerEl.children, {
+        "--card-translateY-offset": "100vh",
+      });
+      gsap.set(
+        cardInfosContainerEl
+          .querySelector(".current--info")
+          .querySelectorAll(".text"),
+        {
+          translateY: "40px",
+          opacity: 0,
+        }
+      );
+      gsap.set([buttons.prev, buttons.next], {
+        pointerEvents: "none",
+        opacity: "0",
+      });
 
-        images.forEach((image) => {
-            imagesLoaded(image, (instance) => {
-                if (instance.isComplete) {
-                    loadedImages++;
-                    let loadProgress = loadedImages / totalImages;
+      images.forEach((image) => {
+        imagesLoaded(image, (instance) => {
+          if (instance.isComplete) {
+            loadedImages++;
+            let loadProgress = loadedImages / totalImages;
 
-                    gsap.to(loaderEl, {
-                        duration: 1,
-                        scaleX: loadProgress,
-                        backgroundColor: `hsl(${loadProgress * 120}, 100%, 50%`,
-                    });
-
-                    if (totalImages === loadedImages) {
-                        gsap.timeline()
-                            .to(".loading__wrapper", {
-                            duration: 0.8,
-                            opacity: 0,
-                            pointerEvents: "none",
-                        })
-                            .call(() => init());
-                    }
-                }
+            gsap.to(loaderEl, {
+              duration: 1,
+              scaleX: loadProgress,
+              backgroundColor: `hsl(${loadProgress * 120}, 100%, 50%`,
             });
+
+            if (totalImages === loadedImages) {
+              gsap
+                .timeline()
+                .to(".loading__wrapper", {
+                  duration: 0.8,
+                  opacity: 0,
+                  pointerEvents: "none",
+                })
+                .call(() => init());
+            }
+          }
         });
+      });
     };
 
     waitForImages();
@@ -341,28 +363,36 @@ const CardComponent = () => {
         </div>
       </div>
 
-      <svg className="icons" style={{ display: 'none' }}>
-        <symbol id="arrow-left" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+      <svg className="icons" style={{ display: "none" }}>
+        <symbol
+          id="arrow-left"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+        >
           <polyline
             points="328 112 184 256 328 400"
             style={{
-              fill: 'none',
-              stroke: '#fff',
-              strokeLinecap: 'round',
-              strokeLinejoin: 'round',
-              strokeWidth: '48px'
+              fill: "none",
+              stroke: "#fff",
+              strokeLinecap: "round",
+              strokeLinejoin: "round",
+              strokeWidth: "48px",
             }}
           />
         </symbol>
-        <symbol id="arrow-right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+        <symbol
+          id="arrow-right"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+        >
           <polyline
             points="184 112 328 256 184 400"
             style={{
-              fill: 'none',
-              stroke: '#fff',
-              strokeLinecap: 'round',
-              strokeLinejoin: 'round',
-              strokeWidth: '48px'
+              fill: "none",
+              stroke: "#fff",
+              strokeLinecap: "round",
+              strokeLinejoin: "round",
+              strokeWidth: "48px",
             }}
           />
         </symbol>
